0e6797779387b7ae99c59ab79ea8893957da0a36
JTZ <876737761@qq.com>
2025-12-23 19:48:36 +0800

    123
    123

--- tutorial-cms/src/main.ts
+++ tutorial-cms/src/main.ts
@@ -1,15 +1,15 @@
-import { serve } from "@hono/node-server";
-import { Hono } from "hono";
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("hello hono");
+app.get('/', (c) => {
+    return c.text('hello hono');
 });
 
 const port = 8080;
 
 serve({
-  fetch: app.fetch,
-  port,
+    fetch: app.fetch,
+    port,
 });
